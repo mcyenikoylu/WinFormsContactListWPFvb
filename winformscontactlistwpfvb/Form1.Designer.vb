85722c87001cfc790b7bdd93c3696a472ae0e9e9
Mehmet Cenk Yeniköylü <mcy@dotnetmcy.com>
2020-09-22 23:42:19 +0300

    vb convert

--- winformscontactlistwpfvb/Form1.Designer.vb
+++ winformscontactlistwpfvb/Form1.Designer.vb
@@ -22,10 +22,54 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.ElementHost1 = New System.Windows.Forms.Integration.ElementHost()
+        Me.UserControl11 = New winformscontactlistwpfvb.UserControl1()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(200, 450)
+        Me.Panel1.TabIndex = 0
+        '
+        'Panel2
+        '
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Panel2.Location = New System.Drawing.Point(200, 350)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(600, 100)
+        Me.Panel2.TabIndex = 1
+        '
+        'ElementHost1
+        '
+        Me.ElementHost1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ElementHost1.Location = New System.Drawing.Point(200, 0)
+        Me.ElementHost1.Name = "ElementHost1"
+        Me.ElementHost1.Size = New System.Drawing.Size(600, 350)
+        Me.ElementHost1.TabIndex = 2
+        Me.ElementHost1.Text = "ElementHost1"
+        Me.ElementHost1.Child = Me.UserControl11
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ElementHost1)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents ElementHost1 As Integration.ElementHost
+    Friend UserControl11 As UserControl1
 End Class
---
+++ winformscontactlistwpfvb/UserControl1.xaml.vb
@@ -0,0 +1,67 @@
+﻿Imports System.Data.SQLite
+Imports System.Data
+Imports System.IO
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Data
+
+Public Class UserControl1
+    Private Sub UserControl_Loaded(sender As Object, e As Windows.RoutedEventArgs)
+        Data("")
+    End Sub
+    Private Sub Data(ByVal searchtext As String)
+        Dim connstr As SQLiteConnection = New SQLiteConnection("Data Source=xpress_entry_sample.db3;Version=3")
+        connstr.Open()
+        Dim cmd As SQLiteCommand = New SQLiteCommand()
+        cmd.Connection = connstr
+
+        If searchtext.Trim() <> "" Then
+            cmd.CommandText = "select u.first_name, u.last_name, u.id_image, b.badge_no, u.login, p.picture, u.email from users u" & " left outer join badges b on u.id = b.user_id" & " left outer join pictures p on p.id = u.picture_id" & " where u.first_name like '%" & searchtext.Trim() & "%'"
+        Else
+            cmd.CommandText = "select u.first_name, u.last_name, u.id_image, b.badge_no, u.login, p.picture, u.email from users u" & " left outer join badges b on u.id = b.user_id" & " left outer join pictures p on p.id = u.picture_id"
+        End If
+
+        Dim rdr As SQLiteDataReader = cmd.ExecuteReader()
+        Dim dt As DataTable = New DataTable()
+        dt.Load(rdr)
+        rdr.Close()
+        connstr.Close()
+        myListBox.ItemsSource = dt.DefaultView
+    End Sub
+
+    Private Sub TextBox_TextChanged(sender As Object, e As Windows.Controls.TextChangedEventArgs)
+        Dim text As String = e.Source.ToString()
+        Dim strSplite As String() = text.Split(" "c)
+
+        If strSplite(0).ToString() = "System.Windows.Controls.TextBox:" Then
+            Data(strSplite(1).ToString())
+        ElseIf strSplite(0).ToString() = "System.Windows.Controls.TextBox" Then
+            Data("")
+        End If
+    End Sub
+End Class
+Public Class BinaryImageConverter
+    Implements IValueConverter
+
+    Public Function Convert(ByVal value As Object,
+                            ByVal targetType As Type,
+                            ByVal parameter As Object,
+                            ByVal culture As System.Globalization.CultureInfo) As Object Implements IValueConverter.Convert
+        'If (TypeOf ((Not (value) Is Nothing) _
+        '            AndAlso value) Is Byte()) Then
+        If value IsNot Nothing AndAlso TypeOf value Is Byte() Then
+
+            Dim ByteArray() As Byte = CType(value, Byte())
+            Dim bmp As BitmapImage = New BitmapImage
+            bmp.BeginInit()
+            bmp.StreamSource = New MemoryStream(ByteArray)
+            bmp.EndInit()
+            Return bmp
+        End If
+
+        Return Nothing
+    End Function
+
+    Public Function ConvertBack(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object Implements IValueConverter.ConvertBack
+        Throw New Exception("The method or operation is not implemented.")
+    End Function
+End Class
